7a7640f2145f0412548e6200dfc1a0cc58454611
amanidreesi67 <amanidreesi67@gmail.com>
2025-12-12 13:07:02 +0530

    4

--- FrontEnd/src/Navigation/HeaderNavigations.js
+++ FrontEnd/src/Navigation/HeaderNavigations.js
@@ -38,6 +38,87 @@ const HeaderNavigation = [{
 },
 {
     id:"Most loved",
-    title:"Most Loved",
+    title:"Most Loved"
     
+},
+{
+    id:"clothing",
+    title:"Clothing",
+    subHeading:[
+        {
+           id:"shirt",
+            title: "Shirt" 
+        },
+        {
+           id:"dresses",
+            title: "Dresses" 
+        },
+         {
+           id:"tops",
+            title: "Tops" 
+        },
+         {
+           id:"co-ord sets",
+            title: "Co-ord Sets" 
+        },
+         {
+           id:"skirts",
+            title: "kirts" 
+        },
+         {
+           id:"bottoms",
+            title: "Bottoms" 
+        },
+         {
+           id:"swimwear",
+            title: "Swimwear" 
+        },
+         {
+           id:"jumpsuits & playsuits",
+            title: "Jumpsuits & Playsuits" 
+        },
+         {
+           id:"jackets & hoodies",
+            title: "Jackets & Hoodies" 
+        },
+         {
+           id:"kids wear",
+            title: "Kids Wear" 
+        }
+    ]
+},
+{
+    id:"shirts",
+    title:"Shirts",
+    subHeading:[
+         {
+           id:"all shirts",
+            title: "All Shirts" 
+        },
+         {
+           id:"new in shirts",
+            title: "New in Shirts" 
+        },
+         {
+           id:"printed shirts",
+            title: "Printed Shirts" 
+        },
+         {
+           id:"embroideried shirt",
+            title: "Embroideried Shirt" 
+        },
+        {
+           id:"embellished shirt",
+            title: "Embellished Shirt" 
+        },
+        {
+           id:"shirts: 2 at 1900",
+            title: "Shirts: 2 at 1900" 
+        },
+        {
+           id:"shirts: 2 at 4900",
+            title: "Shirts: 2 at 4900" 
+        }
+        
+    ]
 }]
