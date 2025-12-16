abab0e87297eabc6647d421962c69f9825947d1e
Paras Negi <paras22negi@gmail.com>
2025-12-16 20:49:22 +0530

    Hero section swiper

--- FrontEnd/src/Home.jsx
+++ FrontEnd/src/Home.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import Header from './Common/Header'
+import React from "react";
+import Header from "./Common/Header";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Autoplay, Pagination } from "swiper/modules";
+import { HeroSectionSwiper } from "./Navigation/HeroSectionImg";
 
 function Home() {
   return (
-    <>Home</>
-  )
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 3500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+        renderBullet: function (index, className) {
+          return `<span class="${className}">
+            <svg class="autoplay-progress" viewBox="0 0 48 48">
+              <circle cx="24" cy="24" r="20"></circle>
+            </svg>
+          </span>`;
+        },
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination]}
+      className="w-full h-[1200px]"
+    >
+      {HeroSectionSwiper.map((data) => (
+        <SwiperSlide key={data.id} className="relative">
+          <img
+            src={data.img}
+            alt={data.title}
+            className="w-full h-[1200px] object-cover "
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
 
-export default Home
+export default Home;
