616fd576fbdbd208258dc4fca01db13430d57a49
shobhitkothiyal2-lang <shobhitkothiyal2@gmail.com>
2025-12-21 12:00:36 +0530

    Added Nodemailer

--- Backend/config/transporter.js
+++ Backend/config/transporter.js
@@ -1,17 +1,21 @@
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config();
+
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port:SMTP_PORT,
-    secure:false,
-    auth:{
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_KEY
-    }
-})
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_KEY,
+  },
+});
+console.log("transporter", transporter)
 
-transporter.verify().then(()=>{
+transporter.verify()
+  .then(() =>{ console.log("✅ Transporter Verified")})
+  .catch((err) =>{ console.log("❌ Transporter Failed:", err.message)});
 
-}) 
+export default transporter;
