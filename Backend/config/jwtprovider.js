8c730bf6ed9e866ba8bea4afe761cf71de0a9b5c
shobhitkothiyal2-lang <shobhitkothiyal2@gmail.com>
2025-12-22 08:33:32 +0530

    secret key update

--- Backend/config/jwtprovider.js
+++ Backend/config/jwtprovider.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const JWT_SECRET_KEY = process.env.JWT_SECRE_KEY
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 const generateToken = (userId)=>{
     const token = jwt.sign({userId},JWT_SECRET_KEY,{expiresIn:'48h'})
