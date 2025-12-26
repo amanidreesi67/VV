d8a4c3640127725190e84e3b6aacfa5e04b61bbc
Paras Negi <paras22negi@gmail.com>
2025-12-26 13:29:48 +0530

    footer

--- FrontEnd/src/Common/Footer.jsx
+++ FrontEnd/src/Common/Footer.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import footerNavigations from "../Navigation/footerNavigations.js";
+import {
+  FaPinterest,
+  FaFacebook,
+  FaInstagram,
+  FaSnapchat,
+  FaYoutube,
+} from "react-icons/fa";
 
 function Footer() {
-  return <div className="w-full bg-[#E8E8E8]">Footer</div>;
+  return (
+    <div className="w-full bg-[#E8E8E8] p-15">
+      <div className="grid grid-cols-4 gap-4">
+        {footerNavigations.map((data) => (
+          <div key={data.id} className="flex flex-col ">
+            <p className="font-bold">{data.title}</p>
+            <br />
+            <ul>
+              {data.subHeading.map((subData) => (
+                <li
+                  key={subData.id}
+                  className="text-gray-400 hover:text-black cursor-pointer group w-fit"
+                >
+                  <a href={subData.link}>{subData.title}</a>
+                  {/* Underline */}
+                  <div className="h-[2px] w-0 bg-black mt-px transition-all duration-300 group-hover:w-full"></div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+        <div className="flex flex-col ">
+          <p className="font-bold">Get in touch</p>
+          <br />
+          <ul>
+            <li className="text-gray-400 hover:text-black cursor-pointer group w-fit">
+              <a href="#">080 6863 5857</a>
+            </li>
+            <li className="text-gray-400 hover:text-black cursor-pointer group w-fit">
+              <a href="#">customerservice@uptownie101.com</a>
+            </li>
+          </ul>
+          <div className="flex gap-4 mt-4 text-gray-500 text-xl">
+            <FaPinterest className="hover:text-black cursor-pointer" />
+            <FaFacebook className="hover:text-black cursor-pointer" />
+            <FaInstagram className="hover:text-black cursor-pointer" />
+            <FaSnapchat className="hover:text-black cursor-pointer" />
+            <FaYoutube className="hover:text-black cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- FrontEnd/src/Navigation/footerNavigations.js
+++ FrontEnd/src/Navigation/footerNavigations.js
@@ -5,10 +5,23 @@ const footerNavigations = [
         subHeading: [
             { id: 1, title: "About Us", link: "/about" },
             { id: 2, title: "Size Chart", link: "/size-chart" },
-            { id: 3, title: "Terms & Conditions", link: "/terms" },
-            { id: 4, title: "Privacy Policy", link: "/privacy" },
-            { id: 5, title: "Return Policy", link: "/return" },
-            { id: 6, title: "Shipping Policy", link: "/shipping" },
+            { id: 3, title: "Careers", link: "/careers" },
+            { id: 4, title: "Blog", link: "/blog" },
+            { id: 5, title: "My Returns", link: "/return" },
+            { id: 6, title: "CAP", link: "/cap" },
+        ]
+    },
+    {
+        id: 2,
+        title: "Policies",
+        subHeading: [
+            { id: 1, title: "Privacy Policy", link: "/privacy-policy" },
+            { id: 2, title: "Shipping & Returns", link: "/shipping-returns" },
+            { id: 3, title: "Payment & Orders", link: "/payment-orders" },
+            { id: 4, title: "Contact Us", link: "/contact-us" },
+            { id: 5, title: "Terms & Conditions", link: "/terms-conditions" },
         ]
     }
 ]
+
+export default footerNavigations;
