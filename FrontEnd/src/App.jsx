381a5f2f5c2af4ec3fdbe8a26a697c36d3bd4f2c
Paras Negi <paras22negi@gmail.com>
2026-01-17 11:14:12 +0530

    errs fixed in imports

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -30,7 +30,7 @@ import OrderSuccess from "./Order/OrderSuccess";
 
 import { useDispatch, useSelector } from "react-redux"; // Import hooks
 import { useEffect } from "react";
-import { getUser } from "./Redux/Auth/Action";
+import { getUser } from "./Redux/Auth/Action.js";
 
 function App() {
   const dispatch = useDispatch();
